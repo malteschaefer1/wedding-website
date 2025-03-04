90409c6a3a9ed01a166d2b86926866c15ba0fc37
Malte Schäfer <malte.schaefer@gmx.net>
2025-03-04 16:17:56 +0100

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -180,27 +180,27 @@ $(document).ready(function () {
         options: {
             class: '',
             // You can pass an ID. If you don't, one will be generated for you
-            id: ''
+            id: 'Maltara2026'
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title:"Hochzeit Maltara ❤️ Bruno",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date(Date.parse("2026-07-25T00:00:00+02:00")),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date(Date.parse("2026-07-27T00:00:00+02:00")),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address:"Wuppertal",
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description:"Wir freuen uns auf Euch :)"
         }
     });
 
